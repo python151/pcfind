198860d2b442c48bc81f6aa3267b11c6763dbf41
Matthew Miller <dogm646@gmail.com>
2020-02-05 22:02:11 +0000

    fixed files

--- pc/PriceCheckerApi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PricecheckerapiConfig(AppConfig):
-    name = 'PriceCheckerApi'
--- pc/PriceCheckerApi/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Search(models.Model):
-    id = models.AutoField(primary_key=True)
-    SearchQuery = models.CharField(max_length=180)
